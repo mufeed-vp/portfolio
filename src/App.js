8e3a8592e183802ad66bd9f38437a064a2ac418f
nufeed-vp <mftechnics1@gmail.com>
2023-08-07 01:19:07 +0530

    Globl changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import AnimatedRoutes from "./components/AnimatedRoutes";
 
 function App() {
   const personalDetails = {
-    name: "Michael Yeates",
-    location: "Loughborough, UK",
-    email: "michael-yeates@outlook.com",
+    name: "Mufeed V P",
+    location: "Kochi, India",
+    email: "mufeedvp93@gmail.com.com",
     availability: "Open for work",
     brand:
-      "My unique blend of technical expertise, creative thinking, and background in psychology allows me to approach each project with a deep understanding of the end user's perspective, resulting in highly effective user-centred digital products.",
+      "I'm a passionate Full Stack Developer with a knack for the MERN stack, Firebase, FastAPI, and Next.js. I thrive on self-motivation, and my commitment to work ethic is unwavering. As a collaborative team player with an open mind, I embrace challenges and seize opportunities to craft exceptional digital experiences. My journey is defined by a dedication to clean and efficient code. Thank you for being a part of my portfolio exploration. Your visit is deeply appreciated and fuels my drive to continue creating meaningful solutions. Let's build something remarkable together!",
   };
 
   return (
