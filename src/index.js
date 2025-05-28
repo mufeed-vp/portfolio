f4a3a31313f9a92bca0a26ab313f3e02a4a1d7ff
Mufeed-vp <mufeedv@voizzit.com>
2025-05-28 10:24:31 +0400

    removed BrowserRouter

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "./pages/404/pageNotFound.css";
 
 import App from "./App";
 
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
